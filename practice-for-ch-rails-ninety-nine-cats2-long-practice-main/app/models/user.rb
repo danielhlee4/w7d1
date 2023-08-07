08748b878ec273a52507613112c85ea5cdcc518f
Dan Lee <danielhlee4@gmail.com>
2023-08-07 14:47:41 -0400

    add to user.rb self.find_by_credentials

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    # attr_reader :password
+    def password=(password)
+        self.password_digest = BCrypt::Password.new(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
 end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/db/schema.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_180553) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
+    t.string "password_digest", null: false
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
